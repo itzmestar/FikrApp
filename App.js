76100aff8aab8e0bc5dc30faf928d9117e242c97
Tarique Anwer <itzmetariq@gmail.com>
2021-02-10 20:12:53 +0530

    solved: remove title bar from login #4

--- App.js
+++ App.js
@@ -6,19 +6,21 @@
  * @flow strict-local
  */
 import React from 'react';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 
-const AppNavigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Login: LoginScreen,
-  },
-  {
-    initialRouteName: 'Login',
-  },
-);
+const Stack = createStackNavigator();
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: true}}/>
+        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-export default createAppContainer(AppNavigator);
+export default App;
